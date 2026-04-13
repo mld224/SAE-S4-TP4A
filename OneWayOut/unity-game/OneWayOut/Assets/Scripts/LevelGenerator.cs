95706b16ee4bff0092452951995502404206d4ce
mchiban <mohammed.chibanibahi@gmail.com>
2026-04-13 17:56:09 +0200

    correction bug convergence 1

--- OneWayOut/unity-game/OneWayOut/Assets/Scripts/LevelGenerator.cs
+++ OneWayOut/unity-game/OneWayOut/Assets/Scripts/LevelGenerator.cs
@@ -35,39 +35,31 @@ public class LevelGenerator : MonoBehaviour
 
     void Start()
     {
-        /* On place le premier embranchement devant le vehicule */
         prochainY = player.transform.position.y + distanceEntreEmbranchements;
-
-        /* On ajoute quelques waypoints droits pour le debut */
-        GenererSegmentDroit(player.transform.position.y + 5f, prochainY - 5f);
+        GenererSegmentDroit(player.transform.position.y + 5f, prochainY - 5f, 0);
     }
 
     void Update()
     {
-        /* Si le vehicule approche du prochain point de generation
-           on cree un nouvel embranchement */
         if (player.transform.position.y + distanceGeneration >= prochainY)
         {
             GenererEmbranchement(prochainY);
             prochainY += distanceEntreEmbranchements;
             nbEmbranchements++;
-
-            /* Nettoyage : supprime les objets trop loin derriere */
             NettoyerAnciensObjets();
         }
     }
 
     /* Genere des waypoints en ligne droite entre startY et endY
        et les ajoute au chemin du vehicule */
-    void GenererSegmentDroit(float startY, float endY)
+    void GenererSegmentDroit(float startY, float endY, float posX)
     {
         List<Transform> waypoints = new List<Transform>();
 
-        /* On place un waypoint tous les 5 unites */
         for (float y = startY; y <= endY; y += 5f)
         {
             GameObject wp = new GameObject("WP_" + y);
-            wp.transform.position = new Vector3(0, y, 0);
+            wp.transform.position = new Vector3(posX, y, 0);
             waypoints.Add(wp.transform);
             anciensObjets.Add(wp);
         }
@@ -78,68 +70,64 @@ public class LevelGenerator : MonoBehaviour
     /* Genere un embranchement a la position Y donnee */
     void GenererEmbranchement(float posY)
     {
-        /* Cree le GameObject de l'embranchement */
-        GameObject embObj = new GameObject("Embranchement_" + nbEmbranchements);
-        embObj.transform.position = new Vector3(0, posY, 0);
+        /* On prend la position X actuelle du vaisseau */
+        float currentX = player.transform.position.x;
 
-        /* Ajoute un Collider2D en trigger pour detecter le vehicule */
+        GameObject embObj = new GameObject("Embranchement_" + nbEmbranchements);
+        embObj.transform.position = new Vector3(currentX, posY, 0);
+
         BoxCollider2D col = embObj.AddComponent<BoxCollider2D>();
         col.isTrigger = true;
         col.size = new Vector2(10f, 2f);
 
-        /* Ajoute le script Embranchement */
         Embranchement emb = embObj.AddComponent<Embranchement>();
         emb.voteManager = voteManager;
         emb.player = player;
 
-        /* Duree du vote : diminue avec le nombre d'embranchements
-           Commence a 10s, descend jusqu'a 3s minimum */
         emb.voteDuration = Mathf.Max(3, 10 - nbEmbranchements);
 
-        /* Le bon choix est aleatoire a chaque embranchement */
         string[] choix = { "A", "B", "C" };
         emb.bonChoix = choix[Random.Range(0, 3)];
 
-        /* Position apres l'embranchement */
         float apresY = posY + longueurChemin;
 
-        /* Genere les 3 chemins */
-        emb.cheminA = CreerChemin(-ecartLateral, posY + 2f, apresY);
-        emb.cheminB = CreerChemin(0, posY + 2f, apresY);
-        emb.cheminC = CreerChemin(ecartLateral, posY + 2f, apresY);
+        /* Les chemins partent de la position actuelle du vaisseau */
+        emb.cheminA = CreerChemin(currentX - ecartLateral, currentX, posY + 2f, apresY);
+        emb.cheminB = CreerChemin(currentX, currentX, posY + 2f, apresY);
+        emb.cheminC = CreerChemin(currentX + ecartLateral, currentX, posY + 2f, apresY);
 
         anciensObjets.Add(embObj);
-
-        /* Genere le segment droit apres la convergence
-           Les 3 chemins se rejoignent au centre puis continuent tout droit */
-        float convergenceY = apresY + 3f;
-        GenererPointConvergence(convergenceY);
-        GenererSegmentDroit(convergenceY + 5f, prochainY + distanceEntreEmbranchements - 5f);
     }
 
     /* Cree une liste de waypoints pour un chemin (A, B ou C)
        decalageX = position horizontale (-5 pour gauche, 0 pour centre, 5 pour droite) */
-    List<Transform> CreerChemin(float decalageX, float startY, float endY)
+    List<Transform> CreerChemin(float targetX, float startX, float startY, float endY)
     {
         List<Transform> waypoints = new List<Transform>();
 
-        /* Point de depart du chemin (le vehicule s'ecarte du centre) */
-        GameObject wp1 = new GameObject("Chemin_" + decalageX + "_start");
-        wp1.transform.position = new Vector3(decalageX, startY + 3f, 0);
+        /* Transition vers le cote choisi */
+        GameObject wp1 = new GameObject("Chemin_" + targetX + "_start");
+        wp1.transform.position = new Vector3((startX + targetX) / 2f, startY + 2f, 0);
         waypoints.Add(wp1.transform);
         anciensObjets.Add(wp1);
 
-        /* Point milieu */
-        GameObject wp2 = new GameObject("Chemin_" + decalageX + "_mid");
-        wp2.transform.position = new Vector3(decalageX, (startY + endY) / 2f, 0);
+        /* Arrive sur la trajectoire choisie */
+        GameObject wp2 = new GameObject("Chemin_" + targetX + "_mid");
+        wp2.transform.position = new Vector3(targetX, (startY + endY) / 2f, 0);
         waypoints.Add(wp2.transform);
         anciensObjets.Add(wp2);
 
-        /* Point de fin (avant convergence) */
-        GameObject wp3 = new GameObject("Chemin_" + decalageX + "_end");
-        wp3.transform.position = new Vector3(decalageX, endY, 0);
+        /* Continue tout droit */
+        GameObject wp3 = new GameObject("Chemin_" + targetX + "_end");
+        wp3.transform.position = new Vector3(targetX, endY, 0);
         waypoints.Add(wp3.transform);
         anciensObjets.Add(wp3);
+
+        /* Continue encore */
+        GameObject wp4 = new GameObject("Chemin_" + targetX + "_far");
+        wp4.transform.position = new Vector3(targetX, endY + 10f, 0);
+        waypoints.Add(wp4.transform);
+        anciensObjets.Add(wp4);
 
         return waypoints;
     }
